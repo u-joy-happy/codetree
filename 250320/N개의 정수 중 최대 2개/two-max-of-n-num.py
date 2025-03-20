b3073f663d45ec4f66df336f62ce8baa850c6943
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-03-20 15:07:02 +0900

    Title: N개의 정수 중 최대 2개, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250320/N개의 정수 중 최대 2개/two-max-of-n-num.py
+++ 250320/N개의 정수 중 최대 2개/two-max-of-n-num.py
@@ -5,15 +5,18 @@ a = list(map(int, input().split()))
 import sys
 
 max_val = -sys.maxsize
+max_idx = -1
 sub_max_val = -sys.maxsize
 
-for i in a :
+for idx, i in enumerate(a) :
     if i >= max_val :
         max_val = i
+        max_idx = idx
 
-a.remove(max_val)
-
-for i in a :
+for idx, i in enumerate(a) :
+    if idx == max_idx :
+        continue
+        
     if i >= sub_max_val and i <= max_val :
         sub_max_val = i
 
