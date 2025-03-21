c5f0475358447c5720fe55e4b859285776add4b6
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-03-21 11:31:21 +0900

    Title: 배열의 평균, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250321/배열의 평균/ave-of-array.py
+++ 250321/배열의 평균/ave-of-array.py
@@ -1,33 +1,16 @@
 arr = [list(map(int, input().split())) for _ in range(2)]
-
-# tot_sum_val = [0]*3
-
-# for i in range(2):
-#     tot_sum_val[0][i] = sum(arr[i])
-#     tot_sum_val[2] += sum(arr[i])
-
-# for j in range(4):
-#     tot_sum_val[1][i] = sum([arr[i][j] for i in range(2)])
-
-avg_horz = [0] *2
-avg_vert = [0] *4
 avg_tot = 0
 
 for i in range(2):
-    avg_horz[i] = sum(arr[i])/4
+    avg_horz = sum(arr[i])/4
+    print(f'{avg_horz:.1f}', end=' ')
     avg_tot += sum(arr[i])/4
-avg_tot /= 2
+print()
 
 for i in range(4):
-    avg_vert[i] = (arr[0][i] + arr[1][i])/2
-
-
-for avg in avg_horz :
-    print(f'{avg:.1f}', end=' ')
+    avg_vert = (arr[0][i] + arr[1][i])/2
+    print(f'{avg_vert:.1f}', end=' ')
 print()
 
-for avg in avg_vert :
-    print(f'{avg:.1f}', end=' ')
-print()
-
+avg_tot /= 2
 print(f'{avg_tot:.1f}')
