2a4ba8c6cdaf1c0c6746100fbc91ae177ae46b81
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-03-26 14:17:27 +0900

    Title: 동전이 있는 위치, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250326/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+
+arr = [[0 for _ in range(n)] for _ in range(n)]
+
+for _ in range(m):
+    r, c = map(int, input().split())
+    arr[r-1][c-1] = 1    
+
+for row in arr :
+    for item in row:
+        print(item, end=' ')
+    print()
