7c72ed01156f210484e802b7bfb41c66f2f0aef7
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-04-11 16:45:46 +0900

    Title: 문자열 계속 지우기, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250411/문자열 계속 지우기/keep-removing-string.py
+++ 250411/문자열 계속 지우기/keep-removing-string.py
@@ -6,9 +6,9 @@ B = input()
 flag = True
 
 while flag :
+    org = A
     for i in range(len(A) - len(B) + 1):
         tar = A[i:i+len(B)]
-        org = A
         if B == tar :
             A = A[:i] + A[i+len(B):]
             break
