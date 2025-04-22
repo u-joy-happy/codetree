a2996927cdce0b9741c7ab2a52f0786689f80a66
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-04-22 16:19:29 +0900

    Title: 대문자로 출력하기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250422/대문자로 출력하기/print-in-capital.py
+++ 250422/대문자로 출력하기/print-in-capital.py
@@ -2,4 +2,8 @@ n = input()
 
 for item in n :
     if item.isalpha() :
-        print(item.upper(), end='')
+        if item >= 'A' and item <= 'Z':
+            print(item, end='')
+        else :
+            code = ord('A') + (ord(item) - ord('a'))
+            print(chr(code), end='')
