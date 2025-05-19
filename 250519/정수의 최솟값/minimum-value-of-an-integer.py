8e180a11c26ecd03036993344f073cb8933362cb
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-05-19 18:15:49 +0900

    Title: 정수의 최솟값, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250519/정수의 최솟값/minimum-value-of-an-integer.py
@@ -0,0 +1,16 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+
+def get_min(a, b, c):
+    min_val = a
+    
+    if min_val > b :
+        min_val = b
+
+    if min_val > c :
+        min_val = c
+    
+    return min_val
+
+print(get_min(a, b, c))
