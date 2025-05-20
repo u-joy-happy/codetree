af1e8b199d844833dff4dd197fc4463349036bc2
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-05-20 13:18:55 +0900

    Title: 함수를 이용한 369 게임, Time: 110ms, Memory: 17MB, Status: Passed - Codetree

--- 250520/함수를 이용한 369 게임/369-games-using-functions.py
+++ 250520/함수를 이용한 369 게임/369-games-using-functions.py
@@ -2,16 +2,20 @@ a, b = map(int, input().split())
 
 # Please write your code here.
 
+def contain369(a) :
+    flag = False
+    for item in str(a) :
+        if item == '3' or item == '6' or item == '9':
+            flag = True
+    return flag
+
 def is_magic_number(a, b) :
     cnt = 0
     for i in range(a, b+1) :
         if i % 3 == 0 :
             cnt += 1
-        elif i % 10 in [3, 6, 9] :
+        elif contain369(i) :
             cnt += 1
-        elif i // 10 in [3, 6, 9] :
-            cnt += 1
-    
     return cnt 
 
 print(is_magic_number(a, b))
