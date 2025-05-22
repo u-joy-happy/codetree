c6affdc53a23a7848af660ec32e0ecba42a10e80
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-05-22 15:29:02 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250522/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
+++ 250522/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.py
@@ -3,23 +3,21 @@ a, b = map(int, input().split())
 # Please write your code here.
 
 def is_decimal(a):
+    if a == 1 :
+        return False
+
     for i in range(2, a) :
         if a % i == 0 :
             return False
     return True
 
 def sum_digit(a) :
-    if a // 10 == 0 :
-        return a % 2 == 0
-    else :
-        return ((a // 10) + (a % 10)) % 2 == 0
+    return ((a // 10) + (a % 10)) % 2 == 0
 
 cnt = 0
 
 for i in range(a, b+1) :
-    if i == 1 :
-        continue
-    elif is_decimal(i) and sum_digit(i) :
+    if is_decimal(i) and sum_digit(i) :
         cnt += 1
 
 print(cnt)
