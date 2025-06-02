372be80c9b1b5aaf3f56be63f3ec9754aa0f62a4
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-06-02 15:47:58 +0900

    Title: 숫자 차례로 출력하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250602/숫자 차례로 출력하기/to-output-numerically.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+# Please write your code here.
+
+def print_asc(n):
+    if n == 0 :
+        return
+    print_asc(n-1)
+    print(n, end=' ')
+
+def print_desc(n):
+    if n == 0 :
+        return
+    print(n, end=' ')
+    print_desc(n-1)
+
+print_asc(n)
+print()
+print_desc(n)
