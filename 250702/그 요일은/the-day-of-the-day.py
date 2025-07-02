5d540e7a7133919cc42ca33d3de4d44ac3f3e454
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-02 17:51:46 +0900

    Title: 그 요일은, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250702/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,19 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+# Please write your code here.
+
+months = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+date1, date2 = 0, 0
+
+for i in range(1, 13):
+    if i < m1 : date1 += months[i]
+    if i < m2 : date2 += months[i]
+
+date1 += d1
+date2 += d2
+
+diff = date2 - date1
+
+print(diff % 7 )
