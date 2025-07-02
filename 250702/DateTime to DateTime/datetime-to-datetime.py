daa1b54e4c9e31ed13e218750f4d8e07d2c6101c
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-02 17:27:48 +0900

    Title: DateTime to DateTime, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250702/DateTime to DateTime/datetime-to-datetime.py
+++ 250702/DateTime to DateTime/datetime-to-datetime.py
@@ -5,4 +5,4 @@ a, b, c = map(int, input().split())
 st_time = 11 + 11 * 60 + 11 * 60 * 24
 ed_time = c + b * 60 + a * 60 * 24
 
-print(ed_time - st_time if ed_time - st_time > 0 else -1 )
+print(ed_time - st_time if ed_time - st_time >= 0 else -1 )
