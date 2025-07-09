9bcf2cf81ddd77af325184ce71e6a45b551f5c3f
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-09 16:26:05 +0900

    Title: 흰검 칠하기, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250709/흰검 칠하기/painting-white-black.py
+++ 250709/흰검 칠하기/painting-white-black.py
@@ -8,17 +8,19 @@ for num, direction in commands:
 
 # Please write your code here.
 
-arr = [0] * 100 * n * 2
+arr = [[0, 0, 'n'] for _ in range(100 * n * 2 + 1)]
 
 cur = (100 * n * 2) // 2
 
 for i in range(n):
     if dir[i] == 'R' :
         for idx in range(cur, cur + x[i]):
-            arr[idx] = abs(arr[idx]) + 1
+            arr[idx][0] += 1
+            arr[idx][2] = 'w'
     else :
         for idx in range(cur, cur - x[i], -1):
-            arr[idx] = -(abs(arr[idx]) + 1)
+            arr[idx][1] += 1
+            arr[idx][2] = 'b'
     cur = idx
 
 w_cnt = 0
@@ -26,13 +28,13 @@ b_cnt = 0
 g_cnt = 0
 
 for item in arr:
-    if item == 0 :
+    if item[0] + item[1] == 0 :
         continue
-    elif item <= -4 or item >= 4 :
+    elif item[0] >= 2 and item[1] >= 2 :
         g_cnt += 1
-    elif item < 0 :
+    elif item[2] == 'w' :
         w_cnt += 1
-    elif item > 0 :
+    elif item[2] == 'b' :
         b_cnt += 1
 
 print(w_cnt, b_cnt, g_cnt)
