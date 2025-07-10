fd52b31ddea111a73797f23c469c82236e298d4e
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-10 11:02:53 +0900

    Title: 연속되는 수 3, Time: 55ms, Memory: 16MB, Status: Passed - Codetree

--- 250710/연속되는 수 3/continuous-number3.py
+++ 250710/연속되는 수 3/continuous-number3.py
@@ -7,9 +7,7 @@ cnt = 0
 max_cnt = 0
 
 for i in range(N):
-    if i == 0 : cnt += 1
-    elif arr[i] < 0 and arr[i-1] < 0 : cnt += 1
-    elif arr[i] > 0 and arr[i-1] > 0 : cnt += 1
+    if i == 0 or arr[i] * arr[i-1] > 0 : cnt +=1
     else : cnt = 1
     
     if cnt >= max_cnt :
