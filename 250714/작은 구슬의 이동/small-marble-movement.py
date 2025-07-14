0f9ed2012cc7774959f1627ec596e9110390afb2
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-14 17:10:13 +0900

    Title: 작은 구슬의 이동, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250714/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,26 @@
+n, t = map(int, input().split())
+r, c, d = input().split()
+r, c = int(r), int(c)
+
+# Please write your code here.
+
+dx = [-1, 0, 0, 1]
+dy = [0, 1, -1, 0]
+
+di = {'L':0, 'U':1, 'D':2, 'R':3}
+dir_num = di[d]
+
+x, y = r-1, c-1
+
+def in_range(x, y) :
+    global n
+    return x >= 0 and x < n and y >= 0 and y < n
+
+for i in range(t) :
+    x, y = x + dx[dir_num], y + dy[dir_num]
+    if not in_range(x, y) :
+        dir_num = 3 - dir_num
+        x, y = x + dx[dir_num], y + dy[dir_num]
+
+print(y, x)
+        
