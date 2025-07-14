47262b9075799f2a6a7adc2367b650317f757cfe
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-14 17:24:23 +0900

    Title: 작은 구슬의 이동, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250714/작은 구슬의 이동/small-marble-movement.py
+++ 250714/작은 구슬의 이동/small-marble-movement.py
@@ -5,12 +5,12 @@ r, c = int(r), int(c)
 # Please write your code here.
 
 dx = [-1, 0, 0, 1]
-dy = [0, 1, -1, 0]
+dy = [0, -1, 1, 0]
 
 di = {'L':0, 'U':1, 'D':2, 'R':3}
 dir_num = di[d]
 
-x, y = r-1, c-1
+y, x = r-1, c-1
 
 def in_range(x, y) :
     global n
@@ -22,5 +22,5 @@ for i in range(t) :
         dir_num = 3 - dir_num
         x, y = x + dx[dir_num], y + dy[dir_num]
 
-print(y, x)
+print(y+1, x+1)
         
