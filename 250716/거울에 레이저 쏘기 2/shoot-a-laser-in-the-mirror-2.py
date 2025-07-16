829c7a9f4d924552c651a7bab56bd5a2497c6228
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-16 15:49:30 +0900

    Title: 거울에 레이저 쏘기 2, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250716/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 250716/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -4,22 +4,13 @@ k = int(input())
 
 # Please write your code here.
 
-dx, dy = [1, 0, -1, 0], [0, 1, 0, -1]
+dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
 
 dir_num = (k-1) // n
 dir_seq = (k-1) % n
 
-def get_start_point():
-    x, y = 0, 0
-    if dir_num == 0:
-        x, y = 0, dir_seq
-    elif dir_num == 1:
-        x, y = dir_seq, 0
-    elif dir_num == 2:
-        x, y = n-1, (n-1)-dir_seq
-    elif dir_num == 3:
-        x, y = (n-1)-dir_seq, n-1
-    return x, y 
+xx = [0, dir_seq, n-1, n-1-dir_seq]
+xy = [dir_seq, n-1, n-1-dir_seq, 0]
 
 def move(x, y):
     global dir_num
@@ -39,10 +30,12 @@ def in_range(x, y):
     return x >= 0 and x < n and y >= 0 and y < n
 
 def laser(x, y):
+    global dir_num
     if not in_range(x, y):
         return 0
     nx, ny = move(x, y)
+    dir_num = (dir_num + 2) % 4
     return laser(nx, ny) + 1
 
-x, y = get_start_point()
+x, y = xx[dir_num], xy[dir_num]
 print(laser(x, y))
