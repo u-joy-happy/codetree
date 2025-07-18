a21e03f081edd6f30a6e43695ff1a76c5b3f7e91
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-18 15:09:26 +0900

    Title: 체크판위에서 2, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250718/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,24 @@
+R, C = map(int, input().split())
+grid = [list(input().split()) for _ in range(R)]
+
+# Please write your code here.
+
+r, c = 0, 0
+goal = ''
+if grid[0][0] == 'W':
+    goal = 'B'
+else :
+    goal = 'W'
+cnt = 0
+
+for i in range(1, R-1):
+    for j in range(1, C-1):
+        if grid[i][j] == goal:
+            goal = 'B' if goal == 'W' else 'W'
+            for k in range(i+1, R-1):
+                for l in range(j+1, C-1):
+                    if grid[k][l] == goal:
+                        cnt += 1
+            goal = 'B' if goal == 'W' else 'W'
+
+print(cnt)
