6a63908613712bd93bd0e04830663c0b0a8cd00a
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-22 11:58:39 +0900

    Title: Carry 피하기 2, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250722/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,25 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+import sys
+
+res = -1
+max_val = -sys.maxsize
+
+def sum_if(a, b, c):
+    if a == 0 and b == 0 and c == 0 :
+        return True
+    elif a % 10 + b % 10 + c % 10 >= 10:
+        return False
+    return sum_if(a // 10, b // 10, c //10)
+
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            a, b, c = arr[i], arr[j], arr[k]
+            if sum_if(a, b, c) :
+                max_val = max(max_val, a+b+c)
+
+print(max_val)
