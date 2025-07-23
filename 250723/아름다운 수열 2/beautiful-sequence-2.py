63f64671a7df9ad355d66908fd47a837fb1785af
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-23 16:39:35 +0900

    Title: 아름다운 수열 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250723/아름다운 수열 2/beautiful-sequence-2.py
+++ 250723/아름다운 수열 2/beautiful-sequence-2.py
@@ -7,13 +7,7 @@ B = list(map(int, input().split()))
 res = 0
 
 for i in range(N-M+1):
-    tmp_b = B * 1
-    for j in range(i, i+M):
-        for k in range(len(tmp_b)):
-            if tmp_b[k] == A[j] :
-                tmp_b.pop(k)
-                break
-    if len(tmp_b) == 0 :
+    if sorted(A[i:i+M]) == sorted(B):
         res += 1
 
 print(res)
