679f1289cf812dc3fa1b92e088f954a1678525dc
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-23 16:55:11 +0900

    Title: 바구니 안의 사탕 2, Time: 50ms, Memory: 17MB, Status: Passed - Codetree

--- 250723/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250723/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -14,7 +14,7 @@ max_val = 0
 for i in range(max(pos)):
     sum_val = 0
     for j in range(N):
-        if i-K <= pos[j] <= i+K:
+        if i-K <= pos[j] <= i+K and 0 <= j <= 100 :
             sum_val += candy[j]
     max_val = max(max_val, sum_val)
 
