49d617a25830497f3b7e49ebfc4b66c9ba53a26d
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-25 16:15:17 +0900

    Title: 개발팀의 능력, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250725/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 250725/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -2,25 +2,24 @@ arr = list(map(int, input().split()))
 
 # Please write your code here.
 
-flag = False
 res = 2 * 1000
+flag = False
+sum_val = sum(arr)
 
 for i in range(5):
     for j in range(5):
         for k in range(5):
-            for l in range(5):
-                for m in range(5):
-                    if i == j or k == i or k == j or l == i or l == j or l == k or m == i or m == j or m == k or m == l: 
-                        continue
-                    sum1 = arr[i] + arr[j]
-                    sum2 = arr[k] + arr[l]
-                    if sum1 == sum2 or sum1 == arr[m] or sum2 == arr[m]:
-                        continue
-                    max_val = max(sum1, sum2, arr[m])
-                    min_val = min(sum1, sum2, arr[m])
-                    diff = max_val - min_val
-                    if res > diff:
-                        res = diff
-                        flag = True
+            if i == j or k == i or k == j :
+                continue
+            sum1 = arr[i] + arr[j]
+            sum2 = sum_val - sum1 - arr[k]
+            max_val = max(sum1, sum2, arr[k])
+            min_val = min(sum1, sum2, arr[k])
+            if sum1 == sum2 or sum2 == arr[k] or sum1 == arr[k]:
+                continue
+            diff = max_val - min_val
+            if res > diff :
+                res = diff
+                flag = True
 
 print(res if flag else -1)
