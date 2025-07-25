6bc5b12a959718d6c76e4e7660a29fc20c931d28
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-25 16:08:43 +0900

    Title: 개발팀의 능력, Time: 53ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250725/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,24 @@
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+res = 2 * 1000
+
+for i in range(5):
+    for j in range(5):
+        for k in range(5):
+            for l in range(5):
+                for m in range(5):
+                    if i == j or k == i or k == j or l == i or l == j or l == k or m == i or m == j or m == k or m == l: 
+                        continue
+                    sum1 = arr[i] + arr[j]
+                    sum2 = arr[k] + arr[l]
+                    if sum1 == sum2 or sum1 == arr[m] or sum2 == arr[m]:
+                        continue
+                    max_val = max(sum1, sum2, arr[m])
+                    min_val = min(sum1, sum2, arr[m])
+                    diff = max_val - min_val
+                    if res > diff:
+                        res = diff
+
+print(res)
