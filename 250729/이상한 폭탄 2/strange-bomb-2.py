2b0e9bf9a020e65cc7fce639f4e364ee85fdf8ce
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-29 16:16:06 +0900

    Title: 이상한 폭탄 2, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 250729/이상한 폭탄 2/strange-bomb-2.py
+++ 250729/이상한 폭탄 2/strange-bomb-2.py
@@ -3,9 +3,11 @@ num = [int(input()) for _ in range(N)]
 
 # Please write your code here.
 max_val = -1
-for i in range(N-K):
-    for j in range(i+1, i+K+1):
-        if num[i] == num[j] :
+for i in range(N):
+    for j in range(i, N):
+        if i == j :
+            continue
+        if abs(i - j) <= K and num[i] == num[j] :
             max_val = max(max_val, num[i])
 
 print(max_val)
