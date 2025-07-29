3fdf995d612cf61790385313b79ab580fee47af5
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-07-29 15:33:07 +0900

    Title: 개발자의 순위, Time: 56ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250729/개발자의 순위/developers-rank.py
@@ -0,0 +1,24 @@
+k, n = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(k)]
+
+# Please write your code here.
+
+res = 0
+
+for i in range(n):
+    for j in range(n):
+        if i == j :
+            continue
+        cnt = 0
+        for l in range(k):
+            idx1, idx2 = 0, 0 
+            for idx, item in enumerate(arr[l]):
+                if item == i : idx1 = idx
+                elif item == j : idx2 = idx
+            if idx1 < idx2 : cnt += 1
+        if cnt == k :
+            res += 1
+
+print(res)
+
+        
