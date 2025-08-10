992091ff8a45d4180612d97362f14b403ba5fbdf
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-08-10 22:50:14 +0900

    Title: 3개의 선 2, Time: 74ms, Memory: 18MB, Status: Passed - Codetree

--- 250810/3개의 선 2/three-lines-2.py
+++ 250810/3개의 선 2/three-lines-2.py
@@ -5,14 +5,36 @@ x, y = list(x), list(y)
 
 # Please write your code here.
 
-flag = False
-for i in range(11):
-    for j in range(11):
-        for k in range(11):
+for i in range(n*2):
+    for j in range(i+1, n*2):
+        for k in range(j+1, n*2):
+            if i < n : 
+                f1, v1 = 'x', x[i]
+            else : 
+                f1, v1 = 'y', y[n-1]
+
+            if j < n : 
+                f2, v2 = 'x', x[j]
+            else : 
+                f2, v2 = 'y', y[j-n]
+
+            if k < n : 
+                f3, v3 = 'x', x[k]
+            else : 
+                f3, v3 = 'y', y[k-n]
+
+            flag = True
             for l in range(n):
-                if x[l] == i or x[l] == j or x[l] == k and y[l] == i or y[l] == j or y[l] == k :
-                    flag = True
-print(1 if flag else 0)
+                if not ((x[l] == v1 and f1 == 'x') or (y[l] == v1 and f1 == 'y') or \
+                (x[l] == v2 and f2 == 'x') or (y[l] == v2 and f2 == 'y') or \
+                (x[l] == v3 and f3 == 'x') or (y[l] == v3 and f3 == 'y')) :
+                    # print(f1, v1, f2, v2, f3, v3)
+                    flag = False
+                    break
+            if flag :
+                print(1)
+                exit()
+print(0)
 
 
 
