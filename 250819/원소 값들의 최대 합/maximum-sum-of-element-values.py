ba689c10d2cadc9b68746a9f269c6adbe0c17773
Hyoju Kim <52645695+u-joy-happy@users.noreply.github.com>
2025-08-19 12:27:54 +0900

    Title: 원소 값들의 최대 합, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250819/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 250819/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -1,5 +1,5 @@
 n, m = map(int, input().split())
-arr = [0] + list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
 # Please write your code here.
 
@@ -10,6 +10,6 @@ for i in range(n):
     sum_val = 0
     for j in range(m):
         sum_val += arr[idx]
-        idx = arr[idx]
+        idx = arr[idx] - 1
     max_val = max(max_val, sum_val)
 print(max_val)
